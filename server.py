552e34a1e1510a3fcd1fde97ae0cba25b02b0748
Jones Sabino <jones.sabino@gmail.com>
2024-07-15 23:14:54 -0300

    changing to vercel

---
+++ server.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Request, Header
+from models import response as res
+from moveo_webhook import MoveoWebhookHandler
+import os
+app = FastAPI()
+
+WEBHOOK_SECRET = str(os.environ.get("WEBHOOK_SECRET"))
+
+@app.post("/{path:path}")
+async def handle_post(request: Request, x_moveo_signature: str = Header(None)):
+    moveo_signature = x_moveo_signature
+    try:
+        request_body = await request.json()
+
+        handler = MoveoWebhookHandler(WEBHOOK_SECRET, debug=True)
+        return handler.process_request(request_body, moveo_signature)
+
+    except Exception as e:
+        return res.err_response(f"Exception occurred: {str(e)}").model_dump(
+            exclude_none=True
+        )
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=80)
